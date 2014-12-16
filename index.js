4b26e25ad37c5d9df3ae00bcc04e7cd7c6e22784
Joseph Frazier <joseph@onsip.com>
2014-12-16 18:44:45 -0500

    prompt for only referer uri, use anon UAs for referee/target

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ global.authFactory = require('sip.js-auth-onsip')(SIP);
 var password = prompt('password');
 global.uris = {
   "referer": prompt('referer'),
-  "referee": prompt('referee'),
-  "referTarget": prompt('referTarget')
+  "referee": false,
+  "referTarget": false,
 };
 
 var streamPromise = SIP.WebRTC.isSupported() && SIP.WebRTC.getUserMedia({audio: true});
@@ -15,55 +15,53 @@ streamPromise.then(function (stream) {
   referer.on('registered', function () {
 
     global.referee = buildUA(uris.referee, stream);
-    referee.on('registered', function () {
+    referee.on('connected', function () {
 
       global.referTarget = buildUA(uris.referTarget, stream);
-      referTarget.on('registered', function () {
+      referTarget.on('connected', function () {
 
-        invite(referer, uris.referee, stream).on('accepted', function () {
-          var refereeSession = this;
-          invite(referer, uris.referTarget, stream).on('accepted', function () {
-            var targetSession = this;
-            refereeSession.off('refer');
-            refereeSession.refer(targetSession);
-          })
-        })
+        invite(referee, uris.referer, stream);
+        referer.once('invite', function (session) {
+          session.accept({media: {stream: stream}}).on('accepted', function () {
+            var refereeSession = this;
+
+            invite(referTarget, uris.referer, stream)
+            referer.once('invite', function (session) {
+              session.accept({media: {stream: stream}}).on('accepted', function () {
+                var targetSession = this;
+                refereeSession.off('refer');
+                refereeSession.refer(targetSession);
+              });
+            });
+          });
+        });
       });
     });
   });
 });
 
   function buildUA (uri, stream) {
-    return new SIP.UA({
-      traceSip: true,
-      uri: uri,
+    var options = {
       //replaces: SIP.C.supported.UNSUPPORTED,
-      authenticationFactory: authFactory(uri, password)
-    })
-    .on('invite', function (session) {
-      console.trace('XXX accepting', session);
-      session.accept({
-        media: {
-          stream: stream
-        }
-      });
-
-      session.on('refer', session.followRefer(function (newSession) {
-        console.trace('XXX followed refer to', newSession);
-      }));
-
-      session.on('terminated', function () {
-        console.trace("XXX TERMINATED", this);
-      });
-    })
+      traceSip: true
+    };
+    if (uri) {
+      options.uri = uri;
+      options.authenticationFactory = authFactory(uri, password);
+    }
+    return new SIP.UA(options);
   }
 
   function invite(from, to, stream) {
-    return from.invite(to, {
+    var session = from.invite(to, {
       media: {
         stream: stream
       }
     });
+
+    session.on('refer', session.followRefer(function (newSession) {
+      console.trace('XXX followed refer to', newSession);
+    }));
   }
 
 global.stop = function () {
